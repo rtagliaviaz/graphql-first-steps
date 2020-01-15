e9fa9e7cf0572e78b60e51a3eab821b76c19260e
robert <robert5638@gmail.com>
2020-01-15 17:53:50 -0400

    'graphql-first-steps'

---
+++ src/database.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export async function connect(){
+  try {
+    await mongoose.connect('mongodb://localhost/mongodbgraphql', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    });
+    console.log('>>> DB is connected');
+  } catch(err){
+    console.log('Something goes wrong!');
+    console.log(err)
+  }
+}
